5f95675ad14ffad5ad7dcc35e519e47030f1f756
Andreas Ohlander <andreas.ohlander@gmail.com>
2025-08-18 12:40:00 +0200

    Initial commit - Project Setup

---
+++ src/main/java/org/josandlin/nacbackend2groupjosandlin/NacBackend2GroupJosandlinApplication.java
@@ -0,0 +1,13 @@
+package org.josandlin.nacbackend2groupjosandlin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacBackend2GroupJosandlinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacBackend2GroupJosandlinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/josandlin/nacbackend2groupjosandlin/NacBackend2GroupJosandlinApplicationTests.java
@@ -0,0 +1,13 @@
+package org.josandlin.nacbackend2groupjosandlin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NacBackend2GroupJosandlinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
