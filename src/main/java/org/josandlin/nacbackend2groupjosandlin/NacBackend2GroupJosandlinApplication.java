9bab0a9ffb721a54eb4e1e68d915b2d668b33730
Andreas Ohlander <andreas.ohlander@gmail.com>
2025-08-18 12:40:59 +0200

    Creating gitflow setup

--- src/main/java/org/josandlin/nacbackend2groupjosandlin/NacBackend2GroupJosandlinApplication.java
+++ src/main/java/org/josandlin/nacbackend2groupjosandlin/NacBackend2GroupJosandlinApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NacBackend2GroupJosandlinApplication {
 
     public static void main(String[] args) {
+        System.out.println("NacBackend2GroupJosandlinApplication starting...");
         SpringApplication.run(NacBackend2GroupJosandlinApplication.class, args);
     }
 
