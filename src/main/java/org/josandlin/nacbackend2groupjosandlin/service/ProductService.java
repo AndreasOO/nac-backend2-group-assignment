00d9d8edb82d6694c0ec95fb81b070ea2d9e87ce
linn <linn.edvardsson@yh.nackademin.se>
2025-08-21 11:54:22 +0200

    Controller method for products

--- src/main/java/org/josandlin/nacbackend2groupjosandlin/service/ProductService.java
+++ src/main/java/org/josandlin/nacbackend2groupjosandlin/service/ProductService.java
@@ -1,15 +1,16 @@
 package org.josandlin.nacbackend2groupjosandlin.service;
 
+import org.josandlin.nacbackend2groupjosandlin.dto.ProductDTO;
 import org.josandlin.nacbackend2groupjosandlin.entity.Product;
 
 import java.util.List;
 
 public interface ProductService {
 /*
-    List<Product> getProducts();
-
     @Transactional
     ProductDTO addProductToOrder(Long productId, Long orderId); <--- ?????????
 
 */
+    List<ProductDTO> getProducts();
+
 }
