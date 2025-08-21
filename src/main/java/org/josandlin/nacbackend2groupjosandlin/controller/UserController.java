e290163935cb6ac2e07acc199a4a34b9c1f52fff
linn <linn.edvardsson@yh.nackademin.se>
2025-08-21 15:13:37 +0200

    admin-view endpoint

--- src/main/java/org/josandlin/nacbackend2groupjosandlin/controller/UserController.java
+++ src/main/java/org/josandlin/nacbackend2groupjosandlin/controller/UserController.java
@@ -2,18 +2,23 @@ package org.josandlin.nacbackend2groupjosandlin.controller;
 
 
 import org.josandlin.nacbackend2groupjosandlin.dto.UserDTO;
-import org.josandlin.nacbackend2groupjosandlin.service.UserServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
-public class LoginController {
+public class UserController {
 
     @GetMapping("/loginView")
     public String login(Model model) {
         model.addAttribute("user", new UserDTO());
         return "loginView";
+    }
+
+    @GetMapping("/admin")
+    public String admin(Model model) {
+        model.addAttribute("user", new UserDTO());
+        return "admin-orders";
     }
 }
