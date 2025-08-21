d46c5c55d911d69af5a2f17f913683bffd303512
Josefin Törner <josefintorner@gmail.com>
2025-08-21 17:09:35 +0200

    endpoint for single product, endpoint created for buying product, no functionality so far

--- src/main/java/org/josandlin/nacbackend2groupjosandlin/controller/ProductController.java
+++ src/main/java/org/josandlin/nacbackend2groupjosandlin/controller/ProductController.java
@@ -6,6 +6,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
@@ -24,6 +26,18 @@ public class ProductController {
         List<ProductDTO> products = productService.getProducts();
         model.addAttribute("products", products);
         return "showAllProducts";
+    }
+
+    @GetMapping("/products/{id}")
+    public String getProduct(Model model, @PathVariable Long id){
+        ProductDTO product = productService.getProductById(id);
+        model.addAttribute("product", product);
+        return "product";
+    }
+
+    @PostMapping("/products/{id}/buy")
+    public String buyProduct(Model model, @PathVariable Long id){
+        return "redirect:/products";
     }
 
 }
