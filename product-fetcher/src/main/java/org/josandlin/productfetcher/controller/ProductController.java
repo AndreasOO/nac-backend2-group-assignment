017015fcb3abc4a02de8201e8bcc280aa2b05451
Josefin Törner <josefintorner@gmail.com>
2025-08-24 11:55:05 +0200

    added restcontroller

---
+++ product-fetcher/src/main/java/org/josandlin/productfetcher/controller/ProductController.java
@@ -0,0 +1,17 @@
+package org.josandlin.productfetcher.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+
+    @GetMapping("/products")
+    public String getProducts() {
+        return "PRODUCTS";
+    }
+}
