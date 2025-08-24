3d929460f5d08d2dc3042eede74660459804d366
Andreas Ohlander <andreas.ohlander@gmail.com>
2025-08-24 21:46:50 +0200

    Added environment variables but could not user them properly in url

--- web-app/src/main/java/org/josandlin/webapp/FakeStoreProductFetcher.java
+++ web-app/src/main/java/org/josandlin/webapp/FakeStoreProductFetcher.java
@@ -4,12 +4,18 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.josandlin.library.dto.ProductDTO;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.net.URL;
 import java.util.List;
 
 
 public class FakeStoreProductFetcher {
+
+
+
+
+
     public static List<ProductDTO> fetchProducts() throws Exception {
         ObjectMapper mapper = new ObjectMapper();
         mapper.registerModule(new JavaTimeModule());
